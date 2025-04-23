9c160da11e6793a4a299b3e8ff10452c2e3989ea
Santiago Méndez <fsmm19@proton.me>
2025-04-23 09:43:24 -0500

    Refactor activate function and clean up comments in extension.js

--- extension.js
+++ extension.js
@@ -1,36 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
- * @param {vscode.ExtensionContext} context
+ * @description This function is called when the extension is activated.
+ * @param {*} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auto-gitignore" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('auto-gitignore.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Auto-GitIgnore!');
-	});
-
-	context.subscriptions.push(disposable);
+  console.log('Auto-GitIgnore is now active!');
 }
 
-// This method is called when your extension is deactivated
+/**
+ * @description This function is called when the extension is deactivated.
+ */
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
