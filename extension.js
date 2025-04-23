e894d9ecabdd57dadcc32fd070f7094559ccba39
Santiago Méndez <fsmm19@proton.me>
2025-04-23 09:44:09 -0500

    Refactor activate function and clean up comments in extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 /**
  * @description This function is called when the extension is activated.
- * @param {*} context
+ * @param {*} context The context object provides information about the extension's environment.
  */
 function activate(context) {
   console.log('Auto-GitIgnore is now active!');
