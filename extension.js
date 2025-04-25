846913d9633b0e480a2f394b6730296f9878bc1c
Santiago Méndez <fsmm19@proton.me>
2025-04-25 17:11:54 -0500

    Add logic to fetch data from the API when activating the extension

--- extension.js
+++ extension.js
@@ -1,9 +1,21 @@
+const url = 'https://www.toptal.com/developers/gitignore/api/list?format=json';
+
 /**
  * @description This function is called when the extension is activated.
  * @param {*} context The context object provides information about the extension's environment.
  */
 function activate(context) {
   console.log('Auto-GitIgnore is now active!');
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error fetching data from API');
+      }
+      return response.json();
+    })
+    .then((data) => console.log('Data fetched successfully:', data))
+    .catch((error) => console.error('Error:', error));
 }
 
 /**
